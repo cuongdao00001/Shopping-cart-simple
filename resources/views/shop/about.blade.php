a27a78329669fe8b8d712be30e07912030d82ce1
cuongdao00001 <104251270+cuongdao00001@users.noreply.github.com>
2022-05-05 14:41:17 +0700

    Add files via upload

--- resources/views/shop/about.blade.php
+++ resources/views/shop/about.blade.php
@@ -5,105 +5,94 @@
 @endsection
 
 @section('content')
-<main id="main" class="main-site">
-
-    <div class="container">
-
-        <div class="wrap-breadcrumb">
-            <ul>
-                <li class="item-link"><a href="{{route('product.index')}}" class="link">Home</a></li>
-                <li class="item-link"><span>Contact us</span></li>
-            </ul>
+<div class="bg-light">
+    <div class="container py-5">
+        <div class="row h-100 align-items-center py-5">
+            <div class="col-lg-6">
+                <h1 class="display-4">Thanks for visiting us</h1>
+                <p class="lead text-muted mb-0">Enjoy Shopping!</p>
+                <p class="lead text-muted">Come here<a href="{{route('product.index')}}" class="text-muted">
+                        <u>Shopping Cart</u></a>
+                </p>
+            </div>
+            <div class="col-lg-6 d-none d-lg-block"><img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" class="img-fluid"></div>
         </div>
     </div>
-
-    <div class="container">
-        <div class="aboutus-info style-center">
-            <b class="box-title">Interesting Facts</b>
-            <p class="txt-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+</div>
+<div class="bg-white py-5">
+    <div class="container py-5">
+        <div class="row align-items-center mb-5">
+            <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+                <h2 class="font-weight-light">Cooperate with more than 50 countries</h2>
+                <p class="font-italic text-muted mb-4">Enjoy shopping for many things</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+            </div>
+            <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://bootstrapious.com/i/snippets/sn-about/img-1.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
         </div>
-
-        <div class="row equal-container">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <div class="aboutus-box-score equal-elem ">
-                    <b class="box-score-title">10000</b>
-                    <span class="sub-title">Items in Store</span>
-                    <p class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text ever since the 1500s...</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <div class="aboutus-box-score equal-elem ">
-                    <b class="box-score-title">90%</b>
-                    <span class="sub-title">Our Customers comeback</span>
-                    <p class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text ever since the 1500s...</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <div class="aboutus-box-score equal-elem ">
-                    <b class="box-score-title">2 million</b>
-                    <span class="sub-title">User of the site</span>
-                    <p class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text ever since the 1500s...</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <div class="aboutus-info style-small-left">
-                    <b class="box-title">What we really do?</b>
-                    <p class="txt-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,</p>
-                </div>
-                <div class="aboutus-info style-small-left">
-                    <b class="box-title">History of the Company</b>
-                    <p class="txt-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <div class="aboutus-info style-small-left">
-                    <b class="box-title">Our Vision</b>
-                    <p class="txt-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,</p>
-                </div>
-                <div class="aboutus-info style-small-left">
-                    <b class="box-title">Cooperate with Us!</b>
-                    <p class="txt-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <div class="aboutus-info style-small-left">
-                    <b class="box-title">Cooperate with Us!</b>
-                    <div class="list-showups">
-                        <label>
-                            <input type="radio" class="hidden" name="showup" id="shoup1" value="shoup1" checked="checked">
-                            <span class="check-box"></span>
-                            <span class='function-name'>Support 24/7</span>
-                            <span class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</span>
-                        </label>
-                        <label>
-                            <input type="radio" class="hidden" name="showup" id="shoup2" value="shoup2">
-                            <span class="check-box"></span>
-                            <span class='function-name'>Best Quanlity</span>
-                            <span class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</span>
-                        </label>
-                        <label>
-                            <input type="radio" class="hidden" name="showup" id="shoup3" value="shoup3">
-                            <span class="check-box"></span>
-                            <span class='function-name'>Fastest Delivery</span>
-                            <span class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</span>
-                        </label>
-                        <label>
-                            <input type="radio" class="hidden" name="showup" id="shoup4" value="shoup4">
-                            <span class="check-box"></span>
-                            <span class='function-name'>Customer Care</span>
-                            <span class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</span>
-                        </label>
-                    </div>
-                </div>
+        <div class="row align-items-center">
+            <div class="col-lg-5 px-5 mx-auto"><img src="https://bootstrapious.com/i/snippets/sn-about/img-2.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+            <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
+                <h2 class="font-weight-light">Fast and convenient</h2>
+                <p class="font-italic text-muted mb-4">Easy shopping over the internet</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
             </div>
         </div>
     </div>
-</main>
+</div>
+
+<div class="bg-light py-5">
+    <div class="container py-5">
+        <div class="row mb-4">
+            <div class="col-lg-5">
+                <h2 class="display-4 font-weight-light">Our team</h2>
+                <p class="font-italic text-muted">We never give up</p>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-xl-3 col-sm-6 mb-5">
+                <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                    <h5 class="mb-0">Nhat Quang</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                    <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-5">
+                <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://placekitten.com/300/300" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                    <h5 class="mb-0">Chi Khanh</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                    <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End-->
+
+            <!-- Team item-->
+            <div class="col-xl-3 col-sm-6 mb-5">
+                <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                    <h5 class="mb-0">Minh Khoi</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                    <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End-->
+        </div>
+    </div>
+</div>
+
+
+<footer class="bg-light pb-5">
+    <div class="container text-center">
+        <p class="font-italic text-muted mb-0">&copy; Copyrights Company.com All rights reserved.</p>
+    </div>
+</footer>
 @endsection
 
