b807702855314d2fa317cf69747bc27bfaa60871
cuongdao00001 <104251270+cuongdao00001@users.noreply.github.com>
2022-05-05 19:58:35 +0700

    Add files via upload

--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -3,27 +3,46 @@
 @section('title')
     Shopping Cart
 @endsection
-
 @section('content')
     @if(Session::has('cart'))
         <div class="row">
             <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
                 <ul class="list-group">
+                    <table class="table">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Picture</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Reduce</th>
+                            <th scope="col">Remove</th>
+                        </tr>
+                        </thead>
+                        <tbody>
                     @foreach($products as $product)
-                        <li class="list-group-item">
-                            <img src="{{$product['item']['imagePath']}}"alt="..." class="img-responsive">
-                            <span class="badge">{{ $product['qty'] }}</span>
-                            <strong>{{ $product['item']['title'] }}</strong>
-                            <span class="label label-success">{{ $product['price'] }}</span>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span></button>
-                                <ul class="dropdown-menu">
-                                    <li><a href="{{ route('product.reduce', ['id' => $product['item']['id']]) }}">Reduce </a></li>
-                                    <li><a href="{{ route('product.remove', ['id' => $product['item']['id']]) }}">Reduce All</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                    @endforeach
+                        <tr>
+                            <th scope="row"></th>
+                            <td><img src="{{$product['item']['imagePath']}}"alt="..." class="img-responsive"></td>
+                            <td>{{ $product['item']['title'] }}</td>
+                            <td>{{ $product['price'] }}</td>
+                            <td>{{ $product['qty'] }}</td>
+                            <td><button type="button" class="btn btn-primary"><a
+                                        href="{{ route('product.reduce', ['id' => $product['item']['id']]) }}">"....."</a><i
+                                        class="far fa-trash-alt"></i></button></td>
+                            <td><button type="button" class="btn btn-primary"><a
+                                        href="{{ route('product.remove', ['id' => $product['item']['id']]) }}">"....."</a><i
+                                        class="far fa-trash-alt"></i></button></td>
+                        </tr>
+                        </tbody>
+                        @endforeach
+                        <table class="table">
+                        <thead class="thead-light">
+                        <tr>
+                        </tr>
+                        </thead>
+                        </table>
                 </ul>
             </div>
         </div>
